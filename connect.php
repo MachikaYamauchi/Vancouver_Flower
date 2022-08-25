b6d05f1a9582c34832e81fa036737b1f8763c31f
Machika Yamauchi <machika111@gmail.com>
2022-08-25 11:45:54 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,14 @@
 <?php
 
-$dbhost = "localhost:8889";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = "Term2_week8Project";
+// $dbhost = "localhost:8889";
+// $dbuser = "root";
+// $dbpass = "root";
+// $dbname = "Term2_week8Project";
+
+$dbhost = "137.184.41.11";
+$dbuser = "myadmin";
+$dbpass = "MYSQL@dmin!12345";
+$dbname = "Vancouver_Flower";
 
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)) {
